6bab6b34359dc7a0709039de2a525f5d5e208a8b
Sandy McArthur <Sandy@McArthur.org>
2014-05-12 18:00:41 -0400

    Working on more robust init scripts

--- Tinc/shared/Tinc.sh
+++ Tinc/shared/Tinc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 CONF=/etc/config/qpkg.conf
 QPKG_NAME="Tinc"
+QPKG_PATH=$(/sbin/getcfg $QPKG_NAME Install_Path -f $CONF)
+TINCD="$QPKG_PATH/tincd"
+LOG_TOOL=/sbin/log_tool
 
 case "$1" in
   start)
@@ -10,10 +13,35 @@ case "$1" in
         exit 1
     fi
     : ADD START ACTIONS HERE
+    # Check for /share/Tinc
+    if [ ! -d /share/Tinc/ ]; then
+	# /share/Tinc/ is missing or not a directory, error
+	$LOG_TOOL -t2 -uTinc -a 'Missing "Tinc" Shared Folder. Tinc start up failed.'
+	$LOG_TOOL -t1 -uTinc -a 'Please create shared folder named "Tinc" with access restricted to admins and restart Tinc.'
+	return -1
+    fi
+    # Check for /share/Tinc/sample config
+    if [ -d /share/Tinc/sample ]; then
+	$LOG_TOOL -t0 -uTinc -a 'Please implement Tinc sample creation.'
+    fi
+    # Start Tinc for echo of /share/Tinc/*/tinc.conf
+    for netConfig in /share/Tinc/*/tinc.conf ; do
+	configDir=`dirname $netConfig`
+	netName=`basename $configDir`
+	# Start tincd -n $netName -c $configDir --pidfile="$configDir/tinc.pid" --logfile="$configDir/tinc.log"
+	$TINCD -n $netName -c $configDir --pidfile="$configDir/tinc.pid" --logfile="$configDir/tinc.log"
+    done
     ;;
 
   stop)
     : ADD STOP ACTIONS HERE
+    # Stop each Tinc Daemon
+    for netConfig in /share/Tinc/*/tinc.conf ; do
+	configDir=`dirname $netConfig`
+	netName=`basename $configDir`
+	# Stop tincd -n $netName -c $configDir --pidfile="$configDir/tinc.pid" --kill
+	$TINCD -n $netName -c $configDir --pidfile="$configDir/tinc.pid" --kill
+    done
     ;;
 
   restart)
